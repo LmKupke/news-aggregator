b9b83d87b2d37e4cbcfa91c9a80cc8b2cad30719
Lincoln Kupke <lincolnkupke@gmail.com>
2016-03-01 16:53:48 -0500

    finished product commit

---
+++ app.rb
@@ -0,0 +1,31 @@
+require "sinatra"
+require 'pry'
+require 'csv'
+
+
+
+get "/" do
+  "<h1>Welcome to the Slack Homepage for Cool Articles!<h1>"
+end
+
+get "/articles" do
+  @articles = CSV.read('articles.csv')
+  erb :index
+end
+
+get '/articles/new' do
+  erb :new_articles
+end
+
+post '/articles/new' do
+
+  @article_title = params['article_title']
+  @article_url = params['article_url']
+  @article_description = params['article_description']
+  
+
+  CSV.open('articles.csv', 'a') do |csv|
+    csv << [@article_title, @article_url, @article_description]
+  end
+  redirect '/articles'
+end
